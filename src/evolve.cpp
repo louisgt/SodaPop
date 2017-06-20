8780c91c165a9c42515fbc3d651bb4941aa23f76
louisgt <louis.gauthier@umontreal.ca>
2017-06-20 10:54:48 -0400

    Minor edits and comments

--- src/evolve.cpp
+++ src/evolve.cpp
@@ -3,7 +3,7 @@
 #include <unistd.h>
 
 /*
-DESCRIPTION: Implementation of microorganism pupulation dynamics and evolution
+DESCRIPTION: Implementation of microorganism population dynamics and evolution
 inspired from "omics" data and protein biophysics.
 
 AUTHOR: LOUIS GAUTHIER
@@ -12,10 +12,9 @@ Version: 1.0
 
 int main(int argc, char *argv[])
 {
-    /* these variables will hold the parameters input (or not) by the user */
-    //Generation counter
-    int GENERATION_CTR = 1;             //default, 1 --> 1st generation
-    int GENERATION_MAX = GENERATION_CTR + 1;    //default, run evolution for only one generation
+    // these variables will hold the parameters input (or not) by the user
+    int GENERATION_CTR = 1;
+    int GENERATION_MAX = GENERATION_CTR + 1;
     int MUTATION_CTR = 0;
     double N=1;
     int node = 1;
@@ -37,7 +36,6 @@ int main(int argc, char *argv[])
     // Wrap everything in a try block
     // errors in input are caught and explained to user
     try { 
-
     // Define the command line object
     TCLAP::CmdLine cmd("Full model of molecular evolution", ' ', "1.0");
 
@@ -52,9 +50,9 @@ int main(int argc, char *argv[])
     TCLAP::ValueArg<std::string> pddgArg("d","pddg","Primordial DDG file",true,"null","filename");
     TCLAP::ValueArg<std::string> startArg("p","pop-desc","Population description file",false,"null","filename");
     TCLAP::ValueArg<std::string> libArg("l","gene-lib","Gene library directory",true,"null","filename");
-
+    // fitness function
     TCLAP::ValueArg<int> fitArg("f","fitness","Fitness function",false,1,"fitness");
-
+    // boolean switch to enable analysis
     TCLAP::SwitchArg analysisArg("a","analysis","Enable analysis scripts", cmd, false);
 
     // Add the arguments to the CmdLine object.
@@ -71,7 +69,7 @@ int main(int argc, char *argv[])
     // Parse the argv array.
     cmd.parse(argc, argv);
 
-    // Get the value parsed by each arg. 
+    // Get values from args. 
     GENERATION_MAX = maxArg.getValue();
     N = popArg.getValue();
     DT = dtArg.getValue();
@@ -83,38 +81,31 @@ int main(int argc, char *argv[])
     genesPath = libArg.getValue();
 
     PolyCell::ff_ = fitArg.getValue();
-
     enableAnalysis = analysisArg.getValue();
 
-    } catch (TCLAP::ArgException &e)  // catch any exceptions
-    { std::cerr << "error: " << e.error() << " for arg " << e.argId() << std::endl; }
-
+    }catch (TCLAP::ArgException &e){
+        std::cerr << "error: " << e.error() << " for arg " << e.argId() << std::endl;}
+    
+    /********************************************/
     std::cout << "Begin ... " << std::endl;
-    /***************************************/
     std::cout << "Initializing DDG matrix ..." << std::endl;
     InitDDGMatrix();
-
     std::cout << "Loading primordial genes file ..." << std::endl;
     LoadPrimordialGenes(geneListFile,genesPath);
-
     std::cout << "Extracting PDDG matrix ..." << std::endl;
     ExtractPDDGMatrix(pddgFile.c_str());
     /********************************************/
 
     std::cout << "Opening starting population snapshot ..." << std::endl;
-    //Open starting population snapshot
-
     std::fstream startsnap (startSnapFile.c_str(),std::ios::in|std::ios::binary);
-    if (!startsnap.is_open())
-    {
+    if (!startsnap.is_open()){
         std::cerr << "File could not be open: "<< startSnapFile << std::endl;
         exit(2);
     }
     
-    //header
+    // header
     int Total_Cell_Count;
     double frame_time;
-
     startsnap.read((char*)(&frame_time),sizeof(double));
     startsnap.read((char*)(&TIME),sizeof(double));
     startsnap.read((char*)(&Total_Cell_Count),sizeof(int));
@@ -126,26 +117,21 @@ int main(int argc, char *argv[])
 
     std::vector <PolyCell> Cell_arr;
     double w_sum = 0;
-    /* WRIGHT-FISHER PROCESS */
+
     // IF POPULATION IS INITIALLY MONOCLONAL
     // CREATE VECTOR WITH N IDENTICAL CELLS
-    // MINOR COMPUTATIONAL PENALTY IS ATTRIBUTION OF BARCODES (BELOW)
+    // MINOR COMPUTATIONAL PENALTY DUE TO REATTRIBUTION OF BARCODES (BELOW)
     if(true){
         std::cout << "Creating a population of " << N << " cells ..." << std::endl;
         PolyCell A(startsnap, genesPath);
         Cell_arr.reserve(N);
         Cell_arr = std::vector <PolyCell>(N,A);
-        for(std::vector<PolyCell>::iterator k = Cell_arr.begin(); k != Cell_arr.end(); ++k)
-        {
+        for(std::vector<PolyCell>::iterator k = Cell_arr.begin(); k != Cell_arr.end(); ++k){
              (*k).ch_barcode(getBarcode());
         } 
     }
     else{
-        // ELSE IT MUST BE POPULATED CELL BY CELL
-        // READ FROM SNAP FILE
-        // THE SNAP FILE CAN BE COPIED TO THE NEW SIMULATION DIRECTORY
-        // INSTEAD OF DUMPING IT
-        // THERE STILL NEEDS TO BE A LOOP TO SUM FITNESS
+        // ELSE IT MUST BE POPULATED CELL BY CELL FROM SNAP FILE
         Cell_arr.reserve(N);
         int i=0;
         std::cout << "Constructing population from source " << startSnapFile.c_str() << " ..." << std::endl;
@@ -157,31 +143,29 @@ int main(int argc, char *argv[])
     }
     startsnap.close();
     std::cout << "Saving initial population snapshot ... " << std::endl;
-    //save initial population snapshot
+    // save initial population snapshot
     sprintf(buffer,"%s/%s.gen%010d.snap",outPath.c_str(),snapFile.c_str(), GENERATION_CTR); 
 
-    //Open snapshot file
+    // Open snapshot file
     std::fstream OUT2(buffer, std::ios::out | std::ios::binary);
-    if ( !OUT2.is_open() ){
+    if (!OUT2.is_open()){
          std::cerr << "Snapshot file could not be opened";
          exit(1);
     }
 
     Total_Cell_Count = Cell_arr.size();
-
     OUT2.write((char*)(&frame_time),sizeof(double));
     OUT2.write((char*)(&TIME),sizeof(double));
     OUT2.write((char*)(&Total_Cell_Count),sizeof(int));
 
-    //dump snapshot of initial population and get sum of fitnesses
-    for(std::vector<PolyCell>::iterator k = Cell_arr.begin(); k != Cell_arr.end(); ++k)
-    {
+    // dump snapshot of initial population and get sum of fitnesses
+    for(std::vector<PolyCell>::iterator k = Cell_arr.begin(); k != Cell_arr.end(); ++k){
         w_sum += (*k).fitness();
         (*k).dumpCell(OUT2);
     } 
     OUT2.close();   
 
-    //Open MUTATION LOG
+    // Open MUTATION LOG
     sprintf(buffer, "out/%s/MUTATION_LOG.%03d",snapFile.c_str(),node);
     std::ofstream MUTATIONLOG(buffer);
     if ( !MUTATIONLOG.is_open() ) {
@@ -190,20 +174,22 @@ int main(int argc, char *argv[])
     }
     std::cout << "Starting evolution ..." << std::endl;
 
-    // WRIGHT FISHER
+    // PSEUDO WRIGHT-FISHER PROCESS
     while(GENERATION_CTR < GENERATION_MAX){
         std::vector<PolyCell> Cell_temp;
+        // reserve 2N to allow overflow and prevent segfault
         Cell_temp.reserve(N*2);
-        //ITERATE THROUGH EVERY CELL IN THE POPULATION
+        // for each cell in the population
         for(std::vector<PolyCell>::iterator j = Cell_arr.begin(); j != Cell_arr.end(); ++j)
         {
-            //fitness of cell j with respect to sum of population fitness
+            // fitness of cell j with respect to sum of population fitness
             double w = (*j).fitness()/w_sum;
-            //probability parameter of binomial distribution
+            // probability parameter of binomial distribution
             std::binomial_distribution<> binCell(N, w);
-            //get number of progeny
+            // number of progeny k is drawn from binomial distribution with N trials and mean w
             int k = binCell(rng);
-       
+            
+            // if nil, the cell will be wiped from the population
             if(k == 0) continue;
 
             // iterator to current available position
@@ -215,16 +201,17 @@ int main(int argc, char *argv[])
             // fill vector with k times the current cell
             std::fill_n(std::back_inserter(Cell_temp),k,(*j));
 
-            // mutation loop
-            while(it < last)
-            {
-                //potentially mutate
+            // after filling with children, go through each one for mutation
+            while(it < last){
+                // potentially mutate
                 if((*it).mrate()*(*it).genome_size() > RandomNumber())
                 {
                     MUTATION_CTR++;
+                    // get selection coefficient s
                     double s = (*it).ranmut_Gene();
 
-                    //save mutation to log
+                    // save beneficial mutations to log
+                    // IDEA: we could save all mutations with abs(s) >= some value x
                     if(s>0){
                         MUTATIONLOG << (*it).barcode().c_str() << "\t";
                         MUTATIONLOG << fixed;
@@ -237,18 +224,17 @@ int main(int argc, char *argv[])
             }
         }
 
-        // upsize population to N
-        while(Cell_temp.size() < N)
-        {
-            //randomly draw from progeny
+        // if the population is below N
+        // randomly draw from progeny to pad
+        while(Cell_temp.size() < N){
             int s = Cell_temp.size();
             std::vector<PolyCell>::iterator j = Cell_temp.begin();
-            Cell_temp.push_back((*(j+RandomNumber()*s)));
+            Cell_temp.emplace_back((*(j+RandomNumber()*s)));
         }
 
-        // or downsize population to N
-        if(Cell_temp.size() > N)
-        {
+        // if the population exceeds N
+        // cells are randomly shuffled and the vector is shrunk to N
+        if(Cell_temp.size() > N){
             auto engine = std::default_random_engine{};
             std::shuffle(Cell_temp.begin(), Cell_temp.end(), engine);
             Cell_temp.resize(N);
@@ -256,42 +242,37 @@ int main(int argc, char *argv[])
 
         Total_Cell_Count = (int)(Cell_temp.size());
         assert(Total_Cell_Count == N);
+        // swap population with initial vector
         Cell_arr.swap(Cell_temp);
 
-        //reset and update w_sum
-        //update Ns and Na for each cell
+        // reset and update w_sum
+        // update Ns and Na for each cell
         w_sum = 0;
-        for(std::vector<PolyCell>::iterator k = Cell_arr.begin(); k != Cell_arr.end(); ++k)
-        {
+        for(std::vector<PolyCell>::iterator k = Cell_arr.begin(); k != Cell_arr.end(); ++k){
             w_sum += (*k).fitness();
             (*k).UpdateNsNa();
         }
         
-        //update generation counter
-        GENERATION_CTR++;
-         
-        //Save population snapshot every DT generations
-        if( (GENERATION_CTR % DT) == 0)
-        {
-             //save population snapshot
+        // update generation counter
+        GENERATION_CTR++; 
+        // save population snapshot every DT generations
+        if( (GENERATION_CTR % DT) == 0){
              sprintf(buffer,"%s/%s.gen%010d.snap",outPath.c_str(),snapFile.c_str(), GENERATION_CTR); 
 
              //Open snapshot file
              std::fstream OUT2(buffer, std::ios::out | std::ios::binary);
-             if ( !OUT2.is_open() ){
+             if (!OUT2.is_open()){
                  std::cerr << "Snapshot file could not be opened";
                  exit(1);
              }
       
              double frame_time = GENERATION_CTR;
-     
              OUT2.write((char*)(&frame_time),sizeof(double));
              OUT2.write((char*)(&TIME),sizeof(double));
              OUT2.write((char*)(&Total_Cell_Count),sizeof(int));
 
              int l=1;
-             for(std::vector<PolyCell>::iterator k = Cell_arr.begin(); k != Cell_arr.end(); ++k)
-             {
+             for(std::vector<PolyCell>::iterator k = Cell_arr.begin(); k != Cell_arr.end(); ++k){
                  (*k).dumpCell(OUT2);
                  //(*k).dump(OUT2,l);
                  l++;
@@ -299,18 +280,16 @@ int main(int argc, char *argv[])
              OUT2.close();
          }
     }
-    /********************************************/
-    std::cout << "Closing events log ..." << std::endl;
+
+    std::cout << "Closing mutation log ..." << std::endl;
     MUTATIONLOG.close();
     std::cout << "Done." << std::endl;
     std::cout << "Total number of mutation events: " << MUTATION_CTR << std::endl;
-    if(enableAnalysis)
-    {
+    // if the user toggled analysis, call shell script
+    if(enableAnalysis){
         std::string command = "/bin/bash tools/barcodes.sh "+snapFile+" "+std::to_string(GENERATION_MAX)+" "+std::to_string(N)+" "+std::to_string(DT);
         const char *cmd = command.c_str();
         system(cmd);
     }
     return 0;
 }
-
-
