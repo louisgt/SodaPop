acab8dcf6acde3b65d28d7d6cadfc562d34dd59c
louisgt <louis.gauthier@umontreal.ca>
2017-06-20 10:11:34 -0400

    Added comments

--- src/PolyCell.h
+++ src/PolyCell.h
@@ -19,69 +19,56 @@ class PolyCell: public Cell
 	    PolyCell(std::fstream&, const std::string&);
 
 	    void UpdateRates();
-
 	    double ranmut_Gene();
-
 	    void change_exprlevel();
-
 	    void dump(std::fstream&, int);
-
         void dumpCell(std::fstream&);
-
 	    void PrintCell(int);
-
 	    void FillGene_L();
-
         void ch_Fitness(double f){fitness_ = f;}
-
+        // Fitness functions
         double flux();
-
         double toxicity();
-
-        const double neutral();
-
-        const double fitness();
-
         double metabolicOutput();
-
+        const double neutral();
+        ///////////////////////
+        const double fitness();
         int Na(){return Total_Na_;}
-
         int Ns(){return Total_Ns_;}
-
         void UpdateNsNa();
 };
 
+// By default the fitness function is set to neutral
 int PolyCell::ff_ = 4;
 
 PolyCell::PolyCell(){}
 PolyCell::PolyCell(std::fstream& f) : Cell(f)
 {
-	//Update current rates
+	// Update current rates
   	this->UpdateRates();  
-  	//Fill gene length array
+  	// Fill gene length array
   	this->FillGene_L();
 }    
 PolyCell::PolyCell(std::fstream& f, const std::string& s) : Cell(f,s)
 {
-	//Update current rates
+	// Update current rates
   	this->UpdateRates();  
-  	//Fill gene length array
+  	// Fill gene length array
   	this->FillGene_L();
 }
 
-//Initialize the cummulative gene length array
+// Initialize the cummulative gene length array
 void PolyCell::FillGene_L()
 {
     int sum = 0;
-
     std::vector<Gene>::iterator i;
-    for(i = Gene_arr_.begin(); i != Gene_arr_.end(); ++i)
-    {
+    for(i = Gene_arr_.begin(); i != Gene_arr_.end(); ++i){
         sum+= (*i).length();
         Gene_L_.push_back(sum);
     }
 }
 
+// FLUX FITNESS FUNCTION
 double PolyCell::flux()
 {
     double f = 0;
@@ -91,6 +78,7 @@ double PolyCell::flux()
     return A_FACTOR/f;
 }
 
+// TOXICITY FITNESS FUNCTION
 double PolyCell::toxicity()
 {
     double f = 0;
@@ -102,28 +90,27 @@ double PolyCell::toxicity()
     else return w;
 }
 
-const double PolyCell::neutral()
-{
-    return 1;
-}
-
+// METABOLIC FLUX FITNESS FUNCTION
 double PolyCell::metabolicOutput()
 {
     double f = 0;
-    for(std::vector<Gene>::iterator i = Gene_arr_.begin(); i != Gene_arr_.end(); ++i){
-        f = f + 1/((*i).functional());
-    }
-    f = A_FACTOR/f;
-
     double t = 0;
     for(std::vector<Gene>::iterator i = Gene_arr_.begin(); i != Gene_arr_.end(); ++i){
+        f = f + 1/((*i).functional());
         t +=(*i).misfolded();
     }
+    f = A_FACTOR/f;
     t = COST*f;
 
     double w = f - t;
     if(w<0) return 0;
     else return w;
+}
+
+// NEUTRAL FITNESS FUNCTION
+const double PolyCell::neutral()
+{
+    return 1;
 }
 
 const double PolyCell::fitness()
@@ -148,17 +135,18 @@ void PolyCell::UpdateRates()
 
 double PolyCell::ranmut_Gene()
 {
-    int L = Gene_L_.back();     //genome size
-    //pick site to mutate
-    //generate a random integer from [0, L]
+    // get genome size
+    int L = Gene_L_.back();
+    // pick random site to mutate
     int site = (int) ( L * (uniformdevptr->doub()));
 
-    //which gene?
+    // find the corresponding gene
     std::vector<Gene>::iterator j = Gene_arr_.begin();
     VectInt_citerator k = Gene_L_.begin();
 
-    if(site >= (*k)){ //random number generated is greater than the cummulative sum
-              //of genes
+    if(site >= (*k)){
+    // random number generated is greater than
+    // the cummulative sum of genes
          for(k = Gene_L_.begin(); k != Gene_L_.end(); ++k){
              if( site<(*k) ) break;
              j++; 
@@ -168,7 +156,6 @@ double PolyCell::ranmut_Gene()
     }
 
     int bp = (int) (3 * (uniformdevptr->doub()));
-
     double wi = fitness();
     (*j).Mutate_BP(site,bp);        
     UpdateRates();
@@ -177,7 +164,7 @@ double PolyCell::ranmut_Gene()
     return s;
 }
 
-//Dump information to binary file
+// Dump cell information to binary file
 void PolyCell::dump(std::fstream& OUT, int cell_index)
 {
     int x;
@@ -202,8 +189,7 @@ void PolyCell::dump(std::fstream& OUT, int cell_index)
     x = (int)(Gene_arr_.size());		 	 
     OUT.write((char*)(&x),sizeof(int));
 
-   for(std::vector<Gene>::iterator i = Gene_arr_.begin(); i != Gene_arr_.end(); ++i)
-   {
+   for(std::vector<Gene>::iterator i = Gene_arr_.begin(); i != Gene_arr_.end(); ++i){
         int gene_nid = (*i).num();
         double s = (*i).e;
         double c = (*i).conc;
@@ -227,7 +213,7 @@ void PolyCell::dump(std::fstream& OUT, int cell_index)
     }
 }
 
-//Dump information to binary file
+// Dump cell summary to binary file
 void PolyCell::dumpCell(std::fstream& OUT)
 {
     int x;
@@ -252,8 +238,7 @@ void PolyCell::UpdateNsNa()
 {
     int new_Na = 0;
     int new_Ns = 0;
-    for(std::vector<Gene>::iterator i = Gene_arr_.begin(); i != Gene_arr_.end(); ++i)
-    {
+    for(std::vector<Gene>::iterator i = Gene_arr_.begin(); i != Gene_arr_.end(); ++i){
         new_Na += (*i).Na();
         new_Ns += (*i).Ns();
     }
@@ -261,32 +246,24 @@ void PolyCell::UpdateNsNa()
     Total_Ns_ = new_Ns;
 }
 
-//Dump CellInformation to Screen
+// Print cell information to stdout
 void PolyCell::PrintCell(int cell_ndx)
 {
       char buffer[140];
-
-      //std::cout << barcode_ << endl;
-     
       sprintf(buffer,"C %6d %6d %12e %12e %d", cell_ndx, ID_, o_mrate_, mrate(), (int)Gene_arr_.size());  
-
       std::cout << buffer << std::endl;
-
-      for(std::vector<Gene>::iterator i = Gene_arr_.begin(); i != Gene_arr_.end(); ++i)
-      {
+      for(std::vector<Gene>::iterator i = Gene_arr_.begin(); i != Gene_arr_.end(); ++i){
         //cout << "X ";
         int gene_nid = (*i).num();
-        double s = (*i).e;
+        double e = (*i).e;
         double c = (*i).conc;
         double dg = -kT*log((*i).dg());
         double ddg = -kT*log((*i).CheckDG());
-
-        int Ns = i->Ns();
-        int Na = i->Na();
+        int Ns = (*i).Ns();
+        int Na = (*i).Na();
            
-        sprintf(buffer,"G %d% 2.2f %10.8f %10.8f %10.8f %d %d ", gene_nid, s, c, dg, ddg, Ns, Na);
-        std::cout << buffer << std::endl;
-        
+        sprintf(buffer,"G %d% 2.2f %10.8f %10.8f %10.8f %d %d ", gene_nid, e, c, dg, ddg, Ns, Na);
+        std::cout << buffer << std::endl;  
       }
       std::cout << std::endl;
 }
