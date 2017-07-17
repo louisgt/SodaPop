1fe6a30591e32b4c498a1dae0e94e768968cc2bc
Louis Gauthier <louis.gauthier@umontreal.ca>
2017-07-17 13:45:51 -0400

    Update Gene.h

--- src/Gene.h
+++ src/Gene.h
@@ -2,6 +2,7 @@
 
 /*SodaPop
 Copyright (C) 2017 Louis Gauthier
+
     SodaPop is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
