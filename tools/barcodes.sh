507e571f3d352adefebdcf7f7e7b2afa519a8c5a
louisgt <louis.gauthier@umontreal.ca>
2018-05-04 12:36:06 -0400

    now works with compressed input and output, parameterized parsing by number of genes

--- tools/barcodes.sh
+++ tools/barcodes.sh
@@ -6,6 +6,7 @@ MAXGEN=$2
 POPSIZE=$3
 DT=$4
 FORMAT=$5
+GENE_COUNT=$6
 
 LONG=0
 
@@ -13,6 +14,7 @@ LONG=0
 rm -rf out/$OUT/barcodes; mkdir out/$OUT/barcodes
 rm -rf out/$OUT/graph; mkdir out/$OUT/graph
 PREFIX=out/$OUT
+HOME=../..
 
 echo Begin analysis.
 
@@ -22,30 +24,43 @@ else
 	echo Using short format.
 fi
 
+echo Gene count = $6.
+
 echo Working in $PREFIX.
+
+cd $PREFIX
 
 echo Extracting barcodes from $PREFIX/snapshots/...
 
 ### CONVERT BINARY SNAPSHOTS TO TEXT FILES
-FILES=$PREFIX/snapshots/*.snap
+FILES=snapshots/*.snap.gz
 for filename in $FILES
 do
-	y=${filename%.001}
-	./sodasnap $filename $y.txt $LONG
+	gunzip $filename
+	y="$(basename $filename .gz)"
+	./$HOME/sodasnap snapshots/$y snapshots/$y.txt $LONG
+	rm snapshots/$y
+	gzip -f snapshots/$y.txt
 done
 
+FACTOR=2
+let "FACTOR += 2*$6"
+
+COL=3
+let "COL += $FORMAT"
+
 #### EXTRACT AND SORT BARCODES
-rm -f $PREFIX/avg_fitness.txt
-FILES=$PREFIX/snapshots/*.snap.txt
+rm -f avg_fitness.txt
+FILES=snapshots/*.snap.txt.gz
 for filename in $FILES
 do
-	y=${filename%%.txt}
-	awk 'NR>1 {print $0}' $filename | awk 'NR%4==2' - | cut -f1 | sort > $PREFIX/barcodes/${y##*/}.barcodes
+	y=${filename%%.txt.gz}
+	gunzip -c $filename | awk 'NR>1 {print $0}' - | awk -v N=$FACTOR 'NR%N==2' - | cut -f1 | sort > barcodes/${y##*/}.barcodes
 	#### SUM POPULATION FITNESS FOR EACH TIME POINT AND DIVIDE BY POP SIZE
 	if [ "$FORMAT" -eq "$LONG" ]; then
-		awk 'NR>1 {print $0}' $filename | awk 'NR%4==2' - | awk -v N=$3 '{sum += $3} END {print sum/N}' - >> $PREFIX/avg_fitness.txt
+		gunzip -c $filename | awk 'NR>1 {print $0}' - | awk -v N=$FACTOR 'NR%N==2' - | awk -v N=$3 -v C=$COL '{sum += $C} END {print sum/N}' - >> avg_fitness.txt
     else
-		awk 'NR>1 {print $0}' $filename | awk 'NR%4==2' - | awk -v N=$3 '{sum += $4} END {print sum/N}' - >> $PREFIX/avg_fitness.txt
+		gunzip -c $filename | awk 'NR>1 {print $0}' - | awk -v N=$FACTOR 'NR%N==2' - | awk -v N=$3 -v C=$COL '{sum += $C} END {print sum/N}' - >> avg_fitness.txt
     fi
 	
 done
@@ -53,45 +68,47 @@ done
 echo Parsing unique barcodes...
 
 #### PARSE UNIQUE BARCODES
-FILES=$PREFIX/barcodes/*.barcodes
+FILES=barcodes/*.barcodes
 for filename in $FILES
 do
 	y=${filename%%.barcodes}
-	uniq -c $filename | awk -F' ' '{t = $1; $1 = $2; $2 = t; print; }' > $PREFIX/barcodes/${y##*/}.unique
+	uniq -c $filename | awk -F' ' '{t = $1; $1 = $2; $2 = t; print; }' > barcodes/${y##*/}.unique
 
 	#### BREAK AT FIXATION POINT IF IT OCCURS
 	if ! $(read -r && read -r)
 	then
 		#### OUTPUT FIXATION GENERATION TO FILE
-	  	echo Fixation at ${y##*/} > $PREFIX/fixation.txt
+	  	echo Fixation at ${y##*/} > fixation.txt
 	  	echo Fixation at ${y##*/}
 	  	break
-	fi < $PREFIX/barcodes/${y##*/}.unique
+	fi < barcodes/${y##*/}.unique
 done
 
-cat $PREFIX/barcodes/$OUT.gen0000000001.snap.unique > $PREFIX/barcodes/start.txt
+cat barcodes/$OUT.gen0000000001.snap.unique > barcodes/start.txt
 
 echo Combining time series...
 
 i=0
 j=1
 
-cat $PREFIX/barcodes/start.txt > $PREFIX/barcodes/series$i.txt
+cat barcodes/start.txt > barcodes/series$i.txt
 
 #### JOIN TIME FRAMES IN A SUITABLE FORMAT
-for filename in `find $PREFIX/barcodes/ -maxdepth 1 -name "*.unique"`
+for filename in `find barcodes/ -maxdepth 1 -name "*.unique"`
 do
-	join -t' ' -e 0 -a 1 -1 1 -2 1 -o 2.2 $PREFIX/barcodes/series$i.txt $filename | paste -d' ' $PREFIX/barcodes/series$i.txt - > $PREFIX/barcodes/series$j.txt
-	rm -f $PREFIX/barcodes/series$i.txt
+	join -t' ' -e 0 -a 1 -1 1 -2 1 -o 2.2 barcodes/series$i.txt $filename | paste -d' ' barcodes/series$i.txt - > barcodes/series$j.txt
+	rm -f barcodes/series$i.txt
 	((i++))
 	((j++))
 done
 
-cat $PREFIX/barcodes/series$i.txt | cut -d " " -f 1,3- > $PREFIX/ALL_generations.txt
+cat barcodes/series$i.txt | cut -d " " -f 1,3- > ALL_generations.txt
 
-rm -f $PREFIX/barcodes/series*.txt
+rm -f barcodes/series*.txt
 
-rm -f $PREFIX/snapshots/*.snap
+rm -f snapshots/*.snap
+
+cd $HOME
 
 #### PLOT RESULTS IN R SCRIPTS
 Rscript tools/polyclonal_structure.R /out/$OUT/ $DT
