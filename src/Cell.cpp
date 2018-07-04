2861df01dea41ce394ea2e563bf6f7a17ef52099
louisgt <louis.gauthier@umontreal.ca>
2018-07-04 12:47:53 -0400

    separated class files in header/source

---
+++ src/Cell.cpp
@@ -0,0 +1,199 @@
+// Gene.cpp
+#include "Cell.h"
+
+Cell::Cell():
+    barcode_(getBarcode()),
+    ID_(0),
+    parent_(0),
+    o_mrate_(0),
+    c_mrate_(0),
+    fitness_(0)
+    {
+        Gene_L_.reserve(GENECOUNTMAX);
+        Gene_arr_.reserve(GENECOUNTMAX);
+    }
+
+// Construct from cell file
+Cell::Cell(std::fstream & cell_in) {
+    char buffer[140];
+    Gene_L_.reserve(GENECOUNTMAX);
+    Gene_arr_.reserve(GENECOUNTMAX);
+    ch_barcode(getBarcode());
+    setParent(0);
+    std::string line;
+    // default genesPath, can be changed by user in command-line
+    std::string genesPath = "files/genes/";
+    while (!cell_in.eof()) {
+        getline(cell_in, line);
+        std::string word;
+        std::istringstream iss(line, std::istringstream:: in );
+        iss >> word;
+        if (word == "genes_path") {
+            iss >> word;
+            genesPath = word.c_str();
+        }
+        if (word == "org_id") {
+            iss >> word;
+            ID_ = atoi(word.c_str());
+        } else if (word == "mrate") {
+            iss >> word;
+            o_mrate_ = atof(word.c_str());
+            c_mrate_ = atof(word.c_str());
+        }
+        else if (word == "fitness") {
+            iss >> word;
+            fitness_ = atof(word.c_str());
+        } else if (word == "G") {
+            //reading gene files; 
+            //concentration and stability from gene file take precedence
+            iss >> word;
+            //open gene file
+            sprintf(buffer, "%s%s.gene", genesPath.c_str(), word.c_str());
+            std::fstream gene_data(buffer);
+            if (!gene_data.is_open()) {
+                std::cerr << "File could not be open: " << buffer << std::endl;
+                exit(2);
+            }
+            Gene A(gene_data,this);
+            Gene_arr_.push_back(A);
+
+            //Check if gene is correctly inserted
+            std::vector <Gene>::iterator i = Gene_arr_.end();
+            i--;
+            // if gene fitness is null, assign a randomly fit value
+            if((*i).f()==0){
+                (*i).ch_f(0.95+randomNumber()*0.02);
+            }
+            gene_data.close();
+        }
+    }
+}
+
+// Constructs from a unit cell stored in binary 
+Cell::Cell(std::fstream & IN,
+    const std::string & genesPath) {
+    Gene_L_.reserve(GENECOUNTMAX);
+    Gene_arr_.reserve(GENECOUNTMAX);
+
+    char buffer[140];
+    int cell_id, cell_index, gene_size;
+    double m, f;
+
+    IN.read((char*)(&cell_index), sizeof(int));
+    IN.read((char*)(&cell_id), sizeof(int));
+    ID_ = cell_id;
+    parent_ = 0;
+
+    //read barcode
+    int l;
+    IN.read((char*) & l, sizeof(int));
+    //construct vector container with nl elements
+    std::vector<char> buf(l);
+    IN.read(&buf[0], l);
+    ch_barcode(std::string().assign(buf.begin(), buf.end()));
+
+    IN.read((char*)(&f), sizeof(double));
+    fitness_ = f;
+    IN.read((char*)(&m), sizeof(double));
+    c_mrate_ = m;
+    IN.read((char*)(&gene_size), sizeof(int));
+
+    //read gene info
+    for (int j = 0; j < gene_size; j++) {
+        double e, c, dg, f, eff;
+        int gene_nid, Ns, Na;
+        std::string DNAsequence;
+
+        IN.read((char*)(&gene_nid), sizeof(int));
+        IN.read((char*)(&e), sizeof(double));
+        IN.read((char*)(&c), sizeof(double));
+        IN.read((char*)(&eff), sizeof(double));
+        IN.read((char*)(&dg), sizeof(double));
+        IN.read((char*)(&f), sizeof(double));
+        IN.read((char*)(&Ns), sizeof(int));
+        IN.read((char*)(&Na), sizeof(int));
+
+        //read DNA sequence
+        int nl;
+        IN.read((char*)&nl, sizeof(int));
+        //construct vector container with nl elements
+        std::vector<char> buf(nl);
+        IN.read(& buf[0], nl);
+        DNAsequence.assign(buf.begin(), buf.end());
+
+        sprintf(buffer, "%s%d.gene", genesPath.c_str(), gene_nid);
+        std::fstream gene_data(buffer);
+        if (!gene_data.is_open()) {
+            std::cerr << "ERROR: Cannot open gene file " << buffer << std::endl;
+            exit(2);
+        }
+        Gene G(gene_data,this);
+        //update gene information
+        G.ch_conc(c);
+        dg = exp(-dg / kT);
+        G.ch_dg(dg);
+        G.ch_eff(eff);
+        G.ch_f(f);
+        G.Update_Sequences(DNAsequence);
+        G.ch_Na(Na);
+        G.ch_Ns(Ns);
+        Gene_arr_.push_back(G);
+    }
+}
+
+// copy constructor
+Cell::Cell(const Cell& C)
+{
+    barcode_ = C.barcode_;
+    ID_ = C.ID_;
+    parent_ = C.parent_;
+    o_mrate_ = C.o_mrate_;
+    c_mrate_ = C.c_mrate_;
+    fitness_ = C.fitness_;
+    Gene_L_ = C.Gene_L_;
+    std::vector < Gene > ::iterator i;
+    for (auto cell_it = C.Gene_arr_.begin(); cell_it != C.Gene_arr_.end(); cell_it++) {
+        Gene A(*cell_it,this);
+        Gene_arr_.push_back(A);
+    }
+}
+
+const double Cell::fitness()
+{
+    return fitness_;
+}
+
+// Initialize the cummulative gene length array
+void Cell::FillGene_L() {
+    int sum = 0;
+    std::vector < Gene > ::iterator i;
+    for (auto cell_it = Gene_arr_.begin(); cell_it != Gene_arr_.end(); ++cell_it) {
+        sum += cell_it->length();
+        Gene_L_.push_back(sum);
+    }
+}
+
+// Return total mutation count
+// spec:
+//  - 0, Ns+Na
+//  - 1, Ns
+//  - 2. Na
+int Cell::total_mutations(const int & spec) {
+    assert((spec < 3) && (spec >= 0));
+
+    int sa = 0;
+    int s = 0;
+    int a = 0;
+
+    for (auto cell_it = Gene_arr_.begin(); cell_it != Gene_arr_.end(); ++cell_it) {
+        int Ns = cell_it -> Ns();
+        int Na = cell_it -> Na();
+        s += Ns;
+        a += Na;
+        sa += (Ns + Na);
+    }
+
+    if (spec == 0) return sa;
+    else if (spec == 1) return s;
+    else return a;
+}
---
+++ src/Gene.cpp
@@ -0,0 +1,373 @@
+// Gene.cpp
+#include "Gene.h"
+
+std::gamma_distribution<> Gene::gamma_ = std::gamma_distribution<>(1.0, 1.0);
+std::normal_distribution<> Gene::normal_ = std::normal_distribution<>(1.0, 1.0);
+
+//Input: gene file
+Gene::Gene(std::fstream& gene_in,Cell *parent)
+{
+    myCell = parent;
+    std::string line;
+    // Read gene file line by line
+    while (!gene_in.eof()){
+        getline(gene_in,line);
+        std::string word;
+        std::istringstream iss(line, std::istringstream::in);
+        iss >> word;
+        if (word == "Gene_NUM"){ 
+            iss>>word; 
+            g_num_ = atoi(word.c_str());
+        }
+        else if (word == "N_Seq"){ 
+            iss>>nucseq_;
+            ln_=nucseq_.length();
+            if((ln_ % 3) != 0){
+                  std::cerr << "Invalid length for nucleotide sequence: " << ln_ << std::endl;
+                  exit(2);
+            }
+            else{
+                  la_ = ln_/3;
+                  std::string aaseq=GetProtFromNuc(nucseq_);
+
+                  //check stop codons in midsequence
+                  std::string::size_type loc = aaseq.find("X", 0 );
+                  if(loc != std::string::npos){
+                        std::cerr << "ERROR: DNA sequence has STOP codons in the middle"<<std::endl;
+                        exit(2);
+                    }           
+            }
+        }
+        else if ( word=="E" ){
+            iss >> word;
+            e_ = atoi(word.c_str());
+        }
+        else if (word == "CONC"){
+            iss >> word;
+    	   conc_ = atof(word.c_str());
+        }
+        else if (word == "DG")
+        { 
+            iss>>word; 
+      	    dg_ = atof(word.c_str());
+            dg_ = exp(-dg_/kT);
+        }
+        else if (word == "F")
+        { 
+            iss>>word; 
+            f_ = atof(word.c_str());
+        }
+        else if (word == "EFF")
+        { 
+            iss>>word; 
+            eff_ = atof(word.c_str());
+        }
+        else if (word == "//"){;}//do nothing
+    }
+    Na_ = 0; //default
+    Ns_ = 0;
+}
+
+// copy constructor
+Gene::Gene(const Gene& G,Cell *parent)
+{
+	myCell = parent;
+    g_num_ = G.g_num_;
+    ln_ = G.ln_;
+    la_ = G.la_;
+    nucseq_ = G.nucseq_;
+    dg_ = G.dg_;
+    f_ = G.f_;
+    conc_ = G.conc_;
+    e_ = G.e_;
+    eff_ = G.eff_;
+    Na_ = G.Na_;
+    Ns_ = G.Ns_;
+}
+
+Gene::~Gene()
+{
+}
+
+//Genes are equal if DNA sequence and concentration are equal.
+bool Gene::operator== (Gene& G) 
+{
+    std::string temp = G.nseq();
+    if ( (temp.compare(nucseq_) == 0) && (conc_ == G.conc_) ) return true;
+    else return false;
+}
+
+// assignment overloading
+Gene& Gene::operator=(const Gene& A)
+{ 
+    if (this != &A){
+        this->g_num_ = A.g_num_;
+        this->ln_ = A.ln_;
+        this->la_ = A.la_;
+        this->dg_ = A.dg_;
+        this->f_ = A.f_;
+        this->conc_ = A.conc_;
+        this->e_ = A.e_;
+        this->eff_ = A.eff_;
+        this->Na_ = A.Na_;
+        this->Ns_ = A.Ns_;
+        (this->nucseq_).assign(A.nucseq_);
+    }
+    return *this;
+}
+
+/*
+This version of the mutation function draws the DDG value from a gaussian distribution
+distribution parameters are hardcoded for now
+*/
+double Gene::Mutate_Stabil_Gaussian(int i, int j)
+{ 
+    if(i>=ln_){
+        std::cerr << "ERROR: Mutation site out of bounds."<< std::endl;
+        exit(2);
+    }       
+
+    //non-synonymous mutation
+    if(randomNumber() <= fNs){
+
+        double temp = Ran_Gaussian(1.0, 1.7);
+        double x = exp(-temp/kT);
+
+        dg_ *= x;
+        Na_ += 1;
+
+        return x;
+    }
+    else{
+        Ns_ += 1;
+        return 1;
+    }
+}
+
+/*
+This version of the mutation function gets the DDG value from the DDG matrix
+input by the user.
+INPUT: 
+    i -> site to mutate
+    j -> bp to mutate to
+*/
+std::string Gene::Mutate_Stabil(int i, int j)
+{ 
+    // extract codon to be mutated
+    int cdn_ndx = (i%3);
+    int cdn_start = i - cdn_ndx; 
+    int resi = cdn_start/3;
+
+    // fetch current codon
+    std::string cdn_curr = nucseq_.substr(cdn_start, 3);
+    // fetch current amino acid
+    int aa_curr = GetIndexFromCodon(cdn_curr);
+    std::string cdn_new = cdn_curr;
+
+    std::string s = PrimordialAASeq.at(0);
+    // get amino acid from WT background   
+    int aa_primo = GetIndexFromAA(s.at(resi));
+
+    // get mutated bp
+    std::string bp = AdjacentBP(cdn_curr.substr(cdn_ndx, 1), j); //new BP
+   
+    // mutate codon
+    cdn_new.replace(cdn_ndx, 1, bp);
+    // check for stop codon
+    cdn_new = n3_to_n3(cdn_new, cdn_curr, cdn_ndx);
+    // get new amino acid
+    int aa_new = GetIndexFromCodon(cdn_new);
+    
+
+    // get DDG value from matrix
+    double x = matrix[g_num_][resi][aa_new-1];
+
+    std::string mutation = std::to_string(g_num_) + '\t' + GetProtFromNuc(cdn_curr) + '\t' + std::to_string(resi) + '\t' + GetProtFromNuc(cdn_new);
+
+    // fetch primordial amino acid
+
+    //Ignore mutations to and from CYSTEINE
+    if( (aa_new==2) || (aa_curr==2)){
+        return "CYSTEINE\tNA\tNA\tNA";
+    }
+
+    //Case unphysical DDG estimate
+    if( x>DDG_min || x<DDG_max){
+        return "UNPHYSICAL\tNA\tNA\tNA";
+    }
+
+    if( aa_curr == aa_new){//SILENT
+          nucseq_.replace(cdn_start, 3, cdn_new);
+          Ns_ += 1;
+          return "SILENT\tNA\tNA\tNA";
+    }
+    else if(aa_primo == aa_new){//REVERT TO WT BACKGROUND
+
+          double x_curr = matrix[g_num_][resi][aa_curr-1];
+          assert( x_curr<DDG_min || x_curr>DDG_max); 
+          
+          dg_ /= x_curr;
+          nucseq_.replace(cdn_start, 3, cdn_new);
+          Na_ += 1;
+          return mutation;
+    }
+    else{//TYPICAL NON-SYNONYMOUS
+
+          double x_curr = matrix[g_num_][resi][aa_curr-1];
+          assert( x_curr<DDG_min || x_curr>DDG_max); 
+
+          // assign new DG value
+          // division accounts for mutation occuring on wildtype identity
+          dg_ /= x_curr;
+          dg_ *= x;
+          if(-kT*log(dg_) > 0){
+            myCell->ch_Fitness(0);
+          }
+          nucseq_.replace(cdn_start, 3, cdn_new);
+          Na_ += 1;
+          return mutation;
+    }
+}
+
+/*
+This version of the mutation function draws the selection coefficient value from a gamma or normal distribution
+*/
+double Gene::Mutate_Select_Dist(int i, int j)
+{ 
+    if(i>=ln_){
+        std::cerr << "ERROR: Mutation site out of bounds."<< std::endl;
+        exit(2);
+    }       
+       
+    //non-synonymous mutation
+    if(randomNumber() <= fNs){
+        double s = RandomNormal();
+        double wf = 1 + s;
+        f_ *= wf;
+        Na_ += 1;
+        return s;
+    }
+    else{
+        Ns_ += 1;
+        return 1;
+    }
+}
+
+/*
+This version of the mutation function gets the selection coefficient value from the DMS matrix
+input by the user.
+INPUT: 
+    i -> site to mutate
+    j -> bp to mutate to
+*/
+std::string Gene::Mutate_Select(int i, int j)
+{ 
+    // extract codon to be mutated
+    int cdn_ndx = (i%3);
+    int cdn_start = i - cdn_ndx; 
+    int resi = cdn_start/3;
+
+    // fetch current codon
+    std::string cdn_curr = nucseq_.substr(cdn_start, 3);
+    // fetch current amino acid
+    int aa_curr = GetIndexFromCodon(cdn_curr);
+    std::string cdn_new = cdn_curr;
+
+    std::string s = PrimordialAASeq.at(g_num_);     
+
+    // get mutated bp
+    std::string bp = AdjacentBP( cdn_curr.substr(cdn_ndx, 1), j); //new BP
+   
+    // mutate codon
+    cdn_new.replace(cdn_ndx, 1, bp);
+    // check for stop codon
+    cdn_new = n3_to_n3(cdn_new, cdn_curr, cdn_ndx);
+    // get new amino acid
+    int aa_new = GetIndexFromCodon(cdn_new);
+    
+    // get selection coefficient from matrix
+    double new_s = matrix[g_num_][resi][aa_new-1];
+
+    std::string mutation = std::to_string(g_num_) + '\t' + GetProtFromNuc(cdn_curr) + '\t' + std::to_string(resi) + '\t' + GetProtFromNuc(cdn_new);
+
+    // fetch primordial amino acid
+
+    if( aa_curr == aa_new){//SILENT
+          nucseq_.replace(cdn_start, 3, cdn_new);
+          Ns_ += 1;
+          return "SILENT\tNA\tNA\tNA";
+    }
+    else{// NON-SYNONYMOUS 
+          // assign new fitness value
+          double new_f = f_ + new_s;
+          f_ = f_ * new_f;
+          nucseq_.replace(cdn_start, 3, cdn_new);
+          Na_ += 1;
+          return mutation;
+    }
+}
+
+void Gene::initGamma(double shape, double scale)
+{
+    Gene::gamma_.param(std::gamma_distribution<>::param_type(shape, scale));
+}
+
+void Gene::initNormal(double mean, double stddev)
+{
+    Gene::normal_.param(std::normal_distribution<>::param_type(mean, stddev));
+}
+
+double Gene::RandomGamma()
+{
+    return Gene::gamma_(g_rng);
+}
+
+double Gene::RandomNormal()
+{
+    return Gene::normal_(g_rng);
+}
+
+// Updates the current DNA sequence
+void Gene::Update_Sequences(const std::string DNAsequence)
+{ 
+    int l = DNAsequence.length();
+
+    if(l != ln_)
+    {
+        std::cerr << "ERROR: Replacing DNA sequence with a non-equal length DNA. "<< std::endl;
+        std::cerr << "Make sure the gene list you provided matches the genes in the cell files."<< std::endl;
+        exit(2);
+    }       
+
+    nucseq_ = DNAsequence;
+}
+
+// from Privalov 1979 (see also: Serohijos & Shakhnovich 2013)
+// Boltzmann probability of the gene product to be in the native state
+double Gene::Pnat()
+{
+    return dg_/(1+dg_);
+}
+
+// Number of functional copies in the cell
+double Gene::functional()
+{
+    return conc_*Pnat();
+}
+
+// Number of misfolded copies in the cell
+double Gene::misfolded()
+{
+    return conc_*(1-Pnat());
+}
+
+// Contribution to normalizing factor based on infinitely stable fold
+double Gene::A_factor()
+{
+    return 1.0/conc_;
+}
+
+inline Cell *Gene::GetCell()
+{
+    return myCell;
+}
